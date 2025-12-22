52a35ad40466b47680a4721cd4c7a311ab680324
Lukas Jungwirth <lukas@brl-jungwirth.at>
2025-12-22 16:11:42 +0100

    fixes error for no input files

--- bin/commands/build.js
+++ bin/commands/build.js
@@ -17,7 +17,12 @@ export async function runBuild() {
 		console.log('↺ No SCSS files found, skipping stylelint');
 	}
 
-	execSync('vite build', { stdio: 'inherit' });
+	if (tsFiles.length > 0 || scssFiles.length > 0) {
+		execSync('vite build', { stdio: 'inherit' });
+	} else {
+		console.log('↺ No source files found, skipping Vite build');
+	}
+
 	execSync('is-wp-plugin-kit compile-mo', { stdio: 'inherit' });
 
 	console.log('✓ Build completed');
