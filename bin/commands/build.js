345a89c3b370bc1e9ed88a4c2072dfeced1201fc
Lukas Jungwirth <lukas@brl-jungwirth.at>
2025-12-22 16:05:16 +0100

    fixes build and dev to runs safely when no scss or ts files

--- bin/commands/build.js
+++ bin/commands/build.js
@@ -1,8 +1,18 @@
 import { execSync } from 'node:child_process';
+import { globSync } from 'glob';
 
 export function runBuild() {
-	execSync('oxlint assets/src/ts', { stdio: 'inherit' });
-	execSync('stylelint "assets/src/scss/**/*.scss" --fix', { stdio: 'inherit' });
+
+	const tsFiles = globSync('assets/src/ts/**/*.ts');
+	if (tsFiles.length > 0) {
+		execSync('oxlint assets/src/ts', { stdio: 'inherit' });
+	}
+
+	const scssFiles = globSync('assets/src/scss/**/*.scss');
+	if (scssFiles.length > 0) {
+		execSync('stylelint "assets/src/scss/**/*.scss" --fix', { stdio: 'inherit' });
+	}
+
 	execSync('vite build', { stdio: 'inherit' });
 	execSync('is-wp-plugin-kit compile-mo', { stdio: 'inherit' });
 
--- bin/commands/dev.js
+++ bin/commands/dev.js
@@ -1,13 +1,20 @@
 import { execSync } from 'node:child_process';
+import { globSync } from 'glob';
 
 export function runDev() {
-	execSync(
-		'concurrently -k ' +
-			'"vite" ' +
-			"\"chokidar 'assets/src/ts/**/*.ts' -c 'oxlint assets/src/ts'\" " +
-			"\"chokidar 'assets/src/scss/**/*.scss' -c 'stylelint \\\"assets/src/scss/**/*.scss\\\" --fix'\" " +
-			"\"chokidar 'assets/src/l10n/**/*.po' -c 'is-wp-plugin-kit compile-mo'\"",
-		{ stdio: 'inherit' }
-	);
+	const commands = ['"vite"'];
+
+	const tsFiles = globSync('assets/src/ts/**/*.ts');
+	if (tsFiles.length > 0) {
+		commands.push("\"chokidar 'assets/src/ts/**/*.ts' -c 'oxlint assets/src/ts'\"");
+	}
+
+	const scssFiles = globSync('assets/src/scss/**/*.scss');
+	if (scssFiles.length > 0) {
+		commands.push("\"chokidar 'assets/src/scss/**/*.scss' -c 'stylelint \\\"assets/src/scss/**/*.scss\\\" --fix'\"");
+	}
+	commands.push("\"chokidar 'assets/src/l10n/**/*.po' -c 'is-wp-plugin-kit compile-mo'\"");
+
+	execSync(`concurrently -k ${commands.join(' ')}`, { stdio: 'inherit' });
 	process.exit(0);
 }
